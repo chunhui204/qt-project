f25bdec8414f5af4ab05ef983c2189e052e1b9b3
chunhui204 <15866613796@163.com>
2018-07-11 15:32:20 +0800

    info

--- algorithm_train.cpp
+++ algorithm_train.cpp
@@ -15,43 +15,41 @@ reference:
 [1] http://www.cplusplus.com/reference/cmath/erf/
 [2] https://zh.wikipedia.org/wiki/%E6%AD%A3%E6%80%81%E5%88%86%E5%B8%83
 */
-double normpdf(const double mean, const double std, const double x)
+double normpdf(const double miu, const double sigma, const double x)
 {
-	
-	return exp(-(x - mean)*(x - mean)*0.5 / (std*std))*RSQRT2PI / std;
+	return exp(-(x - miu)*(x - miu)*0.5 / (sigma*sigma))*RSQRT2PI / sigma;
 }
-double normcdf(const double mean, const double std, const double x)
+double normcdf(const double miu, const double sigma, const double x)
 {
-	return 0.5 + 0.5*erf((x - mean)*RSQRTPI / std);
-
+	return 0.5 + 0.5*erf((x - miu)*RSQRTPI / sigma);
 }
 /*
 *正态分布拟合
 *data必须是一维列向量(n,1)
-*mean，std是外部变量传入，返回拟合高斯的参数
+*miu，sigma是外部变量传入，返回拟合高斯的参数
 */
-void normfit(const Tensor & data, double & mean, double & std)
+void normfit(const Tensor & data, double & miu, double & sigma)
 {
-	//mean
+	//miu
 	double sum = 0;
 	int N = data.rows()*data.cols();
 	for (int i = 0; i < data.rows(); i++)
 	for (int j = 0; j < data.cols(); j++)
 		sum += data(i, j);
-	mean = sum / N;
-	//std
+	miu = sum / N;
+	//sigma
 	sum = 0;
 	for (int i = 0; i < data.rows(); i++)
 	for (int j = 0; j < data.cols(); j++)
-		sum += (data(i, j) - mean)*(data(i, j) - mean);
-	std = sqrt(sum / (N - 1));
+		sum += (data(i, j) - miu)*(data(i, j) - miu);
+	sigma = sqrt(sum / (N - 1));
 }
 /*
 *模型训练
 *data是3维向量，（sample_num,feature_dim, class_num）
 *
 */
-Paramester & gausianTrain(const vector<Tensor> &trainData, double cdTthreshold, double probThreshold, vector<int> group)
+Paramester & gausianTest(const vector<Tensor> &trainData, double cdTthreshold, double probThreshold, vector<int> group)
 {
 	if (trainData.size() < 1){
 		throw "训练数据为空";
@@ -70,18 +68,18 @@ Paramester & gausianTrain(const vector<Tensor> &trainData, double cdTthreshold, 
         //根据已知数据找出每类数据的每个维度特征的均值和标准差
 		for (int dim = 0; dim < featDim; dim++)
 		{
-			double mean = 0;
-			double std = 0;
+			double miu = 0;
+			double sigma = 0;
 			
-			normfit(data.slice(0, -1, dim), mean, std);
-			(*(params.featureMean))(cn, dim) = mean;
-			(*(params.featureStd))(cn, dim) = std;
+			normfit(data.slice(0, -1, dim), miu, sigma);
+			(*(params.featureMean))(cn, dim) = miu;
+			(*(params.featureStd))(cn, dim) = sigma;
 			//通过训练数据找到pdf的门限阈值
 			vector<int> indexs;//不在阈值范围内的sample索引
 			for (int num = 0; num < dataNum; num++)
 			{
-				probp(num, dim) = normpdf(mean, std, data(num, dim));//calc pdf
-				double cdf = normcdf(mean, std, data(num, dim)); //calc cdf
+				probp(num, dim) = normpdf(miu, sigma, data(num, dim));//calc pdf
+				double cdf = normcdf(miu, sigma, data(num, dim)); //calc cdf
 				if (cdf < cdTthreshold || cdf > 1 - cdTthreshold)
 					indexs.push_back(num);
 			}
@@ -99,6 +97,10 @@ Paramester & gausianTrain(const vector<Tensor> &trainData, double cdTthreshold, 
 			for (int num = 0; num < dataNum; num++)
 				probpt(num, g) = temp(num, 0);
 		}
+		Tensor meanProbpt = probpt.mean(1);
+		double probptMiu = 0; 
+		double probptSigma = 0;
+		normfit(meanProbpt, probptMiu, probptSigma);
     }
 
     return params;
