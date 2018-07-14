fc0cbf33b37e1301cac8510abfc80f4d579869a0
chunhui204 <15866613796@163.com>
2018-07-14 08:39:42 +0800

    info

--- algorithm.h
+++ algorithm.h
@@ -12,7 +12,8 @@ struct Paramester
 
 const double RSQRT2PI = 0.39894228040143267793994605993438;//根下2pi分之一
 const double RSQRTPI = 0.56418958354775628694807945156079;//根下pi分之一
-Paramester & gausianTrain(const vector<Tensor> &, float, float, vector<int>);
+Paramester gausianTrain(const vector<Tensor> &, float, float, vector<int>);
+Tensor gausianTest(const Tensor & testdata, const Tensor & featmean, const Tensor & featstan, const Tensor & gate, double cdfthreshold, vector<int> group);
 class Algorithm
 {
 public:
